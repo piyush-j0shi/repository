822a8f17363dc97e7f9a1346d630e759181cce90
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-10 15:04:23 +0000

    absolute and relative path

---
+++ restaurant/target/debug/deps/restaurant-430c2f61623edc37.d
@@ -0,0 +1,7 @@
+/home/null/repository/restaurant/target/debug/deps/librestaurant-430c2f61623edc37.rmeta: src/lib.rs
+
+/home/null/repository/restaurant/target/debug/deps/librestaurant-430c2f61623edc37.rlib: src/lib.rs
+
+/home/null/repository/restaurant/target/debug/deps/restaurant-430c2f61623edc37.d: src/lib.rs
+
+src/lib.rs:
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+/home/null/repository/restaurant/target/debug/librestaurant.rlib: /home/null/repository/restaurant/src/lib.rs
