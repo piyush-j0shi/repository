5d5c7d10bf383d529021e61a82d6dc529383a2d2
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-11 16:01:33 +0000

    using externa packages

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,3 +1,20 @@
+// Using nested paths to clean up Large "use" Lists
+// use std::cmp::Ordering;
+// use std::io
+
+//Those above lines are same as
+//use std::{cmp::Ordering, io};
+
+//One more example
+//use std::io;
+//use std::io::Write;
+
+//We can write like
+//use std::io{self, Write};
+
+//if we want to bring all public items we can use "The Global Operator"
+//use std::collections::*;
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
