bdbbac1f1449561fcd264ceaee3e3d62ae894559
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-11 14:47:47 +0000

    pub. mod and use

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,20 +1,11 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-        fn seat_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
+}
 
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
+use crate::front_of_house::hosting::add_to_waitlist;
 
-    pub fn eat_at_restaurant() {
-        // absolute path
-        crate::front_of_house::hosting::add_to_waitlist();
-
-        // relative path
-        front_of_house::hosting::add_to_waitlist();
-    }
+pub fn eat_at_restaurant() {
+    add_to_waitlist();
 }
