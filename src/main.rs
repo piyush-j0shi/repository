b5385609a38253df6bd1830684146448e82747ce
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-12 15:30:14 +0000

    creating vecors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("main.rs");
+    // it's structure is like `Vec<T>`  and here T stands for type.
+    let v: Vec<i32> = Vec::new();
+
+    // Vec! -> Macro
+    let v1 = vec![1, 2, 3];
+
+    println!("v: {:?}", v);
+    println!("v1: {:?}", v1);
 }
