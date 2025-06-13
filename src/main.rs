1cc47a2d421d75140f81ba75ed93a200d58850e3
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-13 17:38:10 +0000

    adding read_input function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use std::io;
+
+#[derive(Debug)]
+enum Status {
+    Pending,
+    Done,
+}
+
+#[derive(Debug)]
+struct Task {
+    description: String,
+    status: Status,
+}
+
+fn read_input(prompt: &str) -> String {
+    use std::io::Write;
+    print!("{}", prompt);
+    io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    buf.trim().to_string()
+}
+
 fn main() {
-    println!("collection_string.rs");
+    println!("hello world");
 }
