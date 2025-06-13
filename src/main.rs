fa2c0b0e20b7bdc4732a3d620615b35689e06421
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-13 14:57:24 +0000

    string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!(".")
+    // let mut s = String::new();
+
+    let data = "initial content";
+    let s = data.to_string();
+
+    println!("the string is : {s}");
+
+    // we can also write
+    // let s = "initial content".to_string();
+    // OR
+    // let s = String::from("initial content");
+
+    // strings are UTF-8 encoded, so we can include any properly encoded data in them
+    // let hello = String::from("السلام عليكم");
+    // let hello = String::from("Dobrý den");
+    // let hello = String::from("Hello");
+    // let hello = String::from("שלום");
+    // let hello = String::from("नमस्ते");
+    // let hello = String::from("こんにちは");
+    // let hello = String::from("안녕하세요");
+    // let hello = String::from("你好");
+    // let hello = String::from("Olá");
+    // let hello = String::from("Здравствуйте");
+    // let hello = String::from("Hola");
 }
