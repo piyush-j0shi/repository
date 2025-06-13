cda3bd19f7c35903dd3f553a6b143ea590bdfcec
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-13 17:38:38 +0000

    adding add_tasks function

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,18 @@ fn read_input(prompt: &str) -> String {
     buf.trim().to_string()
 }
 
+fn add_task(tasks: &mut Vec<Task>) {
+    let desc = read_input("Enter Task Description : ");
+
+    let task = Task {
+        description: desc,
+        status: Status::Pending,
+    };
+
+    tasks.push(task);
+    println!("task added")
+}
+
 fn main() {
     println!("hello world");
 }
