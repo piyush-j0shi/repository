82ace24d28df7242b072a331d701f5e69dc056c8
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-14 06:55:42 +0000

    watchdogs

--- backyard/src/main.rs
+++
@@ -1,7 +0,0 @@
-use crate::garden::vegetables::Asparagus;
-pub mod garden;
-
-fn main() {
-    let plant = Asparagus {};
-    println!("i'm growing a {plant:?}!");
-}
--- backyard/target/debug/deps/backyard-4668a681d66449dd.d
+++
@@ -1,7 +0,0 @@
-/home/null/repository/backyard/target/debug/deps/backyard-4668a681d66449dd: src/main.rs src/garden.rs src/garden/vegetables.rs
-
-/home/null/repository/backyard/target/debug/deps/backyard-4668a681d66449dd.d: src/main.rs src/garden.rs src/garden/vegetables.rs
-
-src/main.rs:
-src/garden.rs:
-src/garden/vegetables.rs:
