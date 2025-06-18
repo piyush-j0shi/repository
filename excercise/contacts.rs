dff3090e32af393287d320bb54fcf3a112cfbd47
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-18 16:23:06 +0000

    contact.rs

--- excercise/contacts.rs
+++ excercise/contacts.rs
@@ -25,6 +25,12 @@ fn read_input(prompt: &str) -> String {
 
 fn add_contat(contacts: &mut Vec<Contact>) {
     let name = read_input("enter you name : ");
+
+    if contacts.iter().any(|c| c.name == name) {
+        println!("contat already exists");
+        return;
+    }
+
     let contact = read_input("enter the contact no : ");
 
     println!("available contact types : Personal, Work, Other");
@@ -94,6 +100,58 @@ fn search_contact(contacts: &Vec<Contact>) {
     }
 }
 
+fn edit_name(contacts: &mut Vec<Contact>) {
+    view_contacts(contacts);
+
+    if contacts.is_empty() {
+        println!("no contacts available");
+        return;
+    }
+
+    let previous_name = read_input("enter the name you want to change: ");
+    let new_name = read_input("enter new name: ");
+
+    let mut found = false;
+
+    for contact in contacts.iter_mut() {
+        if contact.name == previous_name {
+            contact.name = new_name.clone();
+            found = true;
+            break;
+        }
+    }
+
+    if found {
+        println!("name changed successfully");
+    } else {
+        println!("no contact with that name found");
+    }
+}
+
+fn edit_phone(contacts: &mut Vec<Contact>) {
+    view_contacts(contacts);
+
+    if contacts.is_empty() {
+        println!("no contacts available");
+        return;
+    }
+
+    let name = read_input("enter the name of the contact whose number you want to change: ");
+    let new_number = read_input("enter the new phone number: ");
+
+    if contacts.iter().any(|c| c.phone == new_number) {
+        println!("this phone number is already used by another contact.");
+        return;
+    }
+
+    if let Some(contact) = contacts.iter_mut().find(|c| c.name == name) {
+        contact.phone = new_number;
+        println!("phone number updated successfully");
+    } else {
+        println!("no contact with that name found");
+    }
+}
+
 fn delete_contact(contacts: &mut Vec<Contact>) {
     view_contacts(contacts);
 
@@ -124,7 +182,9 @@ fn main() {
         println!("2. view contact");
         println!("3. search contact");
         println!("4. delete contact");
-        println!("5. exit");
+        println!("5. edit name");
+        println!("6. edit phone");
+        println!("7. exit");
 
         let choice = read_input("enter your choice: ");
 
@@ -133,7 +193,9 @@ fn main() {
             "2" => view_contacts(&conacts),
             "3" => search_contact(&conacts),
             "4" => delete_contact(&mut conacts),
-            "5" => {
+            "5" => edit_name(&mut conacts),
+            "6" => edit_phone(&mut conacts),
+            "7" => {
                 println!("goodbye");
                 break;
             }
