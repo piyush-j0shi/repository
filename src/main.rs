579e42b90439edb24a2f27e284f73685213a758f
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-23 15:23:55 +0000

    result enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use ::std::fs::File;
+
 fn main() {
-    println!("panic.rs");
+    // recoverable errors with `enum Result`
+
+    // enum Result<T, E> {
+    // Ok(T),
+    // Err(E),
+    // }
+
+    // In the above enum `T` represents the type of value returned in case of success,
+    // and `E` represents the type of error in case of failure.
+
+    // if this function succeeds the value `greeting_file_name` will be an instance of `result OK`,
+    // otherwise it will be an instance of `result ERR`
+
+    let _greeting_file_result = File::open("hello.txt");
 }
