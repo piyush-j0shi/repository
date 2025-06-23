0a5391bc4c7cf2e5b4bc71b50c3006e597cc8af8
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-23 15:35:25 +0000

    result and error kind

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use ::std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // recoverable errors with `enum Result`
@@ -14,9 +15,24 @@ fn main() {
     // if this function succeeds the value `greeting_file_name` will be an instance of `result OK`,
     // otherwise it will be an instance of `result ERR`
 
+    // let greeting_file_result = File::open("hello.txt");
+    // let _greeting_file = match greeting_file_result {
+    // Ok(file) => file,
+    // Err(error) => panic!("Problem opening the file: {error:?}"),
+    // };
+
     let greeting_file_result = File::open("hello.txt");
+
     let _greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {error:?}"),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            _ => {
+                panic!("Problem opening the file: {error:?}");
+            }
+        },
     };
 }
