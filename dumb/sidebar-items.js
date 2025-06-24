cd21edf66e4f6b4cc637fe1b9c9fab13d0c9e2dd
piyush-j0shi <piyush-j0shi@users.noreply.github.com>
2025-06-24 15:02:12 +0000

    deploy: bfe3c576a2057aa4ddf266e27f3538a782cd8fc1

--- dumb/sidebar-items.js
+++ dumb/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["main"]};
+window.SIDEBAR_ITEMS = {"fn":["main","read_username_from_file"]};
--- search-index.js
+++ search-index.js
@@ -1,4 +1,4 @@
-var searchIndex = new Map(JSON.parse('[["dumb",{"t":"H","n":["main"],"q":[[0,"dumb"]],"i":"`","f":"{{}b}","D":"`","p":[[1,"unit"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA=","P":[]}]]'));
+var searchIndex = new Map(JSON.parse('[["dumb",{"t":"HH","n":["main","read_username_from_file"],"q":[[0,"dumb"],[2,"alloc::string"],[3,"std::io::error"],[4,"core::result"]],"i":"``","f":"{{}b}{{}{{h{df}}}}","D":"`","p":[[1,"unit"],[5,"String",2],[5,"Error",3],[6,"Result",4,null,1]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAIAEAAAAAAAAQACAA==","P":[]}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
-//{"start":39,"fragment_lengths":[168]}
+//{"start":39,"fragment_lengths":[324]}
