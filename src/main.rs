1d6816680da4116c3d5e4ebee1c1f52f1f4d939d
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-26 15:01:11 +0000

    Type Parametres

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,15 @@
 //    largest
 //}
 
+// when you write a function that works for many types (Or to remove deduplication and to use
+// Generic) so rust needs to know what kind of type you mean and you declare that with `<T>` and it
+// not necessarily to be T but its like a convention you can use whatever you want but you need to
+// make sure one thing it will use only one Letter and its should be capital because rust uses
+// camelcase type
+
+// if you want to remove deduplication from largest we can do it like
+// fn largest<T>(list : *[T]) -> &T{}
+
 fn main() {
     //    let number_list = vec![34, 50, 25, 100, 65];
     //    let result = largest_i32(&number_list);
