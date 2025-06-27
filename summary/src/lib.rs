4193c540622b76c3bcc05351c500cfa80873840d
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-27 16:03:31 +0000

    new crate

---
+++ summary/src/lib.rs
@@ -0,0 +1,9 @@
+// So basically this whole story revolves around one thing which is Trait is going to be a list of methods
+// and if your type has those methods
+// then you can use that trait for that type, But that type needs ti implement those methods
+
+// now where to check if the type has those methods or not if custom then you know othervise
+// look for docs or rust compiler will let you know baby.
+
+// one more thing which is do we need to implement all the traits and answer is I don't know
+// if there are default methods then leave it as it is othervise compiler will let you know once again.
