c46fd626661ace7a0d19a0d55cbcddfb0377f13b
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-27 17:06:49 +0000

    default and overridden traits

--- summary/src/lib.rs
+++ summary/src/lib.rs
@@ -1,15 +1,31 @@
 // So basically this whole story revolves around one thing which is Trait is going to be a list of methods
 // and if your type has those methods
 // then you can use that trait for that type, But that type needs ti implement those methods
-
 // now where to check if the type has those methods or not if custom then you know othervise
 // look for docs or rust compiler will let you know baby.
-
 // one more thing which is do we need to implement all the traits and answer is I don't know
 // if there are default methods then leave it as it is othervise compiler will let you know once again.
 
+// Primary trait
+
+// pub trait Summary {
+//    fn summarize(&self) -> String;
+// }
+
+// trait with default implementation
+// pub trait Summary {
+//    fn summarize(&self) -> String {
+//        String::from("(Read More)")
+//    }
+// }
+
+// Both Default and Overridden
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
 }
 
 pub struct NewsArticle {
@@ -19,11 +35,19 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-}
+// default trait implementation
+// impl Summary for NewsArticle {}
+
+// If you don’t implement the method in your impl, Rust uses the default from the trait.
+// If you do implement it, you are overriding the default.
+
+// if I want to override the default implementation
+
+// impl Summary for NewsArticle {
+//    fn summarize(&self) -> String {
+//        format!("{}, by {} ({})", self.headline, self.author, self.location)
+//    }
+// }
 
 pub struct SocialPost {
     pub username: String,
@@ -32,8 +56,15 @@ pub struct SocialPost {
     pub repost: bool,
 }
 
+// impl Summary for SocialPost {
+//    fn summarize(&self) -> String {
+//        format!("{}: {}", self.username, self.content)
+//    }
+// }
+
+// overriding and keeping default both
 impl Summary for SocialPost {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
     }
 }
--- summary/src/main.rs
+++ summary/src/main.rs
@@ -1,12 +1,41 @@
+// You can only implement a trait for a type if you own the trait OR the type (or both).
+// For Ex : if I denife a try to use a trait like display on a vec! than i can not use this because
+// i do not know any of these that means these both comes from standard library so I can not use
+// this and also I do not have these in scope so I can use whatever I have in Scope.
+
 use summary::{SocialPost, Summary};
+// use summary::{NewsArticle, Summary};
 
 fn main() {
+    // Using Trait
+
+    //    let post = SocialPost {
+    //        username: String::from("horse_ebooks"),
+    //        content: String::from("of course, as you already know, people"),
+    //        reply: false,
+    //        repost: false,
+    //    };
+    //    println!("1 new social post  : {}", post.summarize());
+
+    // using default trait
+
+    // let article = NewsArticle {
+    //    headline: String::from("Penguins win Stanley Cup Championship!"),
+    //    location: String::from("Pittsburgh, PA, USA"),
+    //    author: String::from("Iceburgh"),
+    //    content: String::from(
+    //        "The Pittsburgh Penguins once again are the best hockey team in the NHL.",
+    //    ),
+    //};
+    // println!("New article available! {}", article.summarize());
+
+    // using both
+
     let post = SocialPost {
         username: String::from("horse_ebooks"),
-        content: String::from("of course, as you already know, people"),
+        content: String::from("of course, as you probably already know, people"),
         reply: false,
         repost: false,
     };
-
-    println!("1 new social post  : {}", post.summarize());
+    println!("1 new social post: {}", post.summarize());
 }
