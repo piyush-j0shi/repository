301c533de61f7e5718457a75d3176ca797a12b10
piyush-j0shi <thefarmguy011@gmail.com>
2025-06-28 12:23:35 +0000

    trait.rs

---
+++ summary/target/debug/deps/summary-686092ec78b4285a.d
@@ -0,0 +1,5 @@
+/home/null/repository/summary/target/debug/deps/libsummary-686092ec78b4285a.rmeta: src/main.rs
+
+/home/null/repository/summary/target/debug/deps/summary-686092ec78b4285a.d: src/main.rs
+
+src/main.rs:
---
+++ summary/target/debug/deps/summary-7486f8d21bee11bc.d
@@ -0,0 +1,5 @@
+/home/null/repository/summary/target/debug/deps/libsummary-7486f8d21bee11bc.rmeta: src/main.rs
+
+/home/null/repository/summary/target/debug/deps/summary-7486f8d21bee11bc.d: src/main.rs
+
+src/main.rs:
