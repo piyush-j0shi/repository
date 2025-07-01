b1cb23708db5c6ec57d138375f7aba6d30c0ccb8
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-01 16:05:13 +0000

    lifetime ellipsion

--- src/main.rs
+++ src/main.rs
@@ -93,10 +93,39 @@ fn main() {
 
     // Lifetime annotations in struct definitions
 
-    let novel = String::from("call me someone. some years ago.....");
+    let novel = String::from("call me Ishmael. Some years ago.....");
     let first_sentence = novel.split('.').next().unwrap();
     let i = ImportantExcerpt {
         part: first_sentence,
     };
     println!("ImportantExcerpt is : {:?}", i);
+
+    // Lifetime Elision
+
+    // The first rule is that the compiler assigns a lifetime parameter to each parameter that’s a reference.
+    // In other words, a function with one parameter gets one lifetime parameter: fn foo<'a>(x: &'a i32);
+    // a function with two parameters gets two separate lifetime parameters: fn foo<'a, 'b>(x: &'a i32, y: &'b i32); and so on.
+
+    // The second rule is that, if there is exactly one input lifetime parameter,
+    // that lifetime is assigned to all output lifetime parameters: fn foo<'a>(x: &'a i32) -> &'a i32.
+
+    // The third rule is that, if there are multiple input lifetime parameters,
+    // but one of them is &self or &mut self because this is a method,
+    // the lifetime of self is assigned to all output lifetime parameters.
+    // This third rule makes methods much nicer to read and write because fewer symbols are necessary.
+
+    // Example
+
+    //  We’ll apply these rules to figure out the lifetimes of the references
+    //  in the signature of the first_word function The signature starts without any lifetimes associated with the references:
+    // fn first_word(s: &str) -> &str {
+
+    // Then the compiler applies the first rule, which specifies that each parameter gets its own lifetime.
+    // We’ll call it 'a as usual, so now the signature is this:
+    // fn first_word<'a>(s: &'a str) -> &str {
+
+    // The second rule applies because there is exactly one input lifetime.
+    // The second rule specifies that the lifetime of the one input parameter gets assigned to the output lifetime,
+    // so the signature is now this:
+    // fn first_word<'a>(s: &'a str) -> &'a str {
 }
