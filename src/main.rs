e6ec6ebf82b6ee8d65cf82a56cd0ede9ef40bdc1
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-01 15:33:02 +0000

    lifetimes annotations and lifetime function signatures and lifetime returning refrences

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,76 @@
+// every reference in rust has a lifetime, which is the scope for which that reference is valid
+
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// function with lifetimes
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
 fn main() {
-    println!("traits.rs");
+    // here we are denoting the lifetime of r with 'a and x with 'b so just to thing the r has
+    // the inner 'b block is much smaller than the outer 'a lifetime block. At compile time,
+    // Rust compares the size of the two lifetimes and sees that r has a lifetime of 'a but that
+    // it refers to memory with a lifetime of 'b. The program is rejected because 'b is shorter
+    // than 'a: the subject of the reference doesn’t live as long as the reference so the code
+    // won't work.
+
+    // let r;                // ---------+-- 'a
+    //          |
+    // {                     //          |
+    //  let x = 5;           // -+-- 'b  |
+    //  r = &x;              //  |       |
+    // }                     // -+       |
+    //          |
+    // println!("r: {r}");   //          |
+    //                       // ---------+
+
+    // But here the code will work because the lifetime 'b is greater than 'a, because rust knows
+    // that the reference of r will always be valid in x.
+
+    // let x = 5;            // ----------+-- 'b
+    //                       //           |
+    // let r = &x;           // --+-- 'a  |
+    //                       //   |       |
+    // println!("r: {r}");   //   |       |
+    //                       // --+       |
+    //                       //    ----------+
+
+    // same goes here rust needs to know that the reference of r will always be valid in x
+
+    // let string1 = String::from("abcd");
+    // let string2 = "xyz";
+    // let result = longest(string1.as_str(), string2);
+    // println!("the longest string is : {result}")
+
+    // lifetime annotations
+    // &i32 // a reference
+    // &'a i32 // a reference with an explicit lifetime
+    // &'a mut i32 // a mutable reference with an explicit lifetime
+
+    // using the function here
+
+    // let string1 = String::from("abcd");
+    // let string2 = "xyz";
+    // let result = longest(string1.as_str(), string2);
+    // println!("the longest string is : {result}");
+
+    // this is valid
+
+    // let string1 = String::from("long string is long");
+    // {
+    //     let string2 = String::from("abcd");
+    //     let result = longest(string1.as_str(), string2.as_str());
+    //     println!("the longest string is : {result}")
+    // }
 }
