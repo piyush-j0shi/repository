2ea64ee38b8b87708485f9f1a297d11a42a00a8e
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-02 17:42:23 +0000

    trait

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
-// 4. generic struct
-#[derive(Debug)]
-struct Container<T> {
-    value: T,
+// 6. Trait
+trait Descriable {
+    fn new(val: String) -> Self;
+    fn describe(&self) -> String;
 }
 
-impl<T> Container<T> {
-    fn new(value: T) -> Self {
-        Container { value }
+struct Person {
+    name: String,
+}
+struct Car {
+    model: String,
+}
+
+impl Descriable for Person {
+    fn new(val: String) -> Self {
+        Person { name: val }
     }
 
-    fn get(&self) -> &T {
-        &self.value
+    fn describe(&self) -> String {
+        format!("person named : {}", self.name)
+    }
+}
+
+impl Descriable for Car {
+    fn new(val: String) -> Self {
+        Car { model: val }
+    }
+
+    fn describe(&self) -> String {
+        format!("car model : {}", self.model)
     }
 }
 
 fn main() {
-    let new_values = Container::new(-2);
-    println!("{:?}", new_values.get());
+    let person = Person::new(String::from("person1"));
+    println!("{:?}", person.describe());
+
+    let car = Car::new(String::from("porsche 911 gt3 rs"));
+    println!("{:?}", car.describe());
 }
