86f13c730590388f95abb91c26c379bd95e45d49
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-02 17:49:14 +0000

    i did something

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,29 @@
-// 6. Trait
 trait Descriable {
-    fn new(val: String) -> Self;
-    fn describe(&self) -> String;
+    fn new() -> Self;
+    fn add(&mut self, val: String);
+    fn describe(&self);
 }
 
 struct Person {
-    name: String,
-}
-struct Car {
-    model: String,
+    name: Vec<String>,
 }
 
 impl Descriable for Person {
-    fn new(val: String) -> Self {
-        Person { name: val }
+    fn new() -> Self {
+        Person { name: Vec::new() }
     }
 
-    fn describe(&self) -> String {
-        format!("person named : {}", self.name)
-    }
-}
-
-impl Descriable for Car {
-    fn new(val: String) -> Self {
-        Car { model: val }
+    fn add(&mut self, value: String) {
+        self.name.push(value);
     }
 
-    fn describe(&self) -> String {
-        format!("car model : {}", self.model)
+    fn describe(&self) {
+        println!("person named : {:?}", self.name)
     }
 }
 
 fn main() {
-    let person = Person::new(String::from("person1"));
-    println!("{:?}", person.describe());
-
-    let car = Car::new(String::from("porsche 911 gt3 rs"));
-    println!("{:?}", car.describe());
+    let mut person = Person::new();
+    person.add(String::from("hello"));
+    person.describe();
 }
