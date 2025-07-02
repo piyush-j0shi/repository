346ba98444ab4bb3bf8149d3c3c5bc29e139c8c9
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-02 16:55:34 +0000

    vec+enum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// 1. vec + enum
+#[derive(Debug)]
+enum Status {
+    Active,
+    Inactive,
+    Pending,
+}
+
 fn main() {
-    println!("combinations.md");
+    // 1. vec +
+    let statues: Vec<Status> = vec![Status::Active, Status::Inactive, Status::Pending];
+
+    for status in statues {
+        match status {
+            Status::Active => println!("active"),
+            Status::Inactive => println!("inactive"),
+            Status::Pending => println!("pending"),
+        }
+    }
 }
