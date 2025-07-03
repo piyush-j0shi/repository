a73c59ea12eaec75823d07d6f7be824705ce073f
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-03 16:03:03 +0000

    generics

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,15 @@ struct TaskList {
     tasks: Vec<Task>,
 }
 
+#[derive(Debug)]
+// struct ItemsList<T> {
+//     items : T,
+// }
+
+struct ItemList<T> {
+    items: Vec<T>,
+}
+
 impl Task {
     fn new(name: &str, description: &str) -> Self {
         Task {
@@ -43,6 +52,48 @@ impl TaskList {
     fn add_task(&mut self, task: Task) {
         self.tasks.push(task);
     }
+
+    // fn todo_count(&self) -> usize {
+    //     self.tasks.iter()
+    //         .filter(|task| matches!(task.status, TaskStatus::Todo))
+    //         .count()
+    // }
+
+    // fn inprogress_count(&self) -> usize {
+    //     self.tasks.iter()
+    //         .filter(|task| matches!(task.status, TaskStatus::InProgress))
+    //         .count()
+    // }
+
+    // fn completed_count(&self) -> usize {
+    //     self.tasks.iter()
+    //         .filter(|task| matches!(task.status, TaskStatus::Completed))
+    //         .count()
+    // }
+}
+
+// impl <T> ItemsList<T> {
+//     fn new(item : T) -> Self {
+//         ItemsList { items: item }
+//     }
+// }
+
+impl<T> ItemList<T> {
+    fn new() -> Self {
+        ItemList { items: Vec::new() }
+    }
+
+    fn add(&mut self, item: T) {
+        self.items.push(item);
+    }
+
+    fn count(&self) -> usize {
+        self.items.len()
+    }
+
+    fn get_all(&self) -> &Vec<T> {
+        &self.items
+    }
 }
 
 fn main() {
@@ -67,4 +118,28 @@ fn main() {
     let mut tasklist = TaskList::new();
     tasklist.add_task(task);
     println!("{:#?}", tasklist);
+
+    // let todotask = tasklist.todo_count();
+    // let inprogresstask = tasklist.inprogress_count();
+    // let completedtask = tasklist.completed_count();
+
+    // println!("todo task count : {}", todotask);
+    // println!("inprogress task count : {}", inprogresstask);
+    // println!("completed task count : {}", completedtask);
+
+    // let itemlist = ItemsList::new(3);
+    // println!("{:?}", itemlist.items);
+
+    let mut itemlist = ItemList::new();
+    itemlist.add("item");
+    itemlist.add("item");
+    itemlist.add("item");
+    itemlist.add("item");
+
+    let itemcount = itemlist.count();
+    let getallitem = itemlist.get_all();
+
+    println!("items : {:#?}", itemlist);
+    println!("items : {}", itemcount);
+    println!("items : {:#?}", getallitem);
 }
