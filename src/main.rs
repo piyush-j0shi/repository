bd1cb99f1a91f704433f89fa6c02f38926b312d3
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-03 16:44:23 +0000

    i_think_something

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[allow(clippy::uninlined_format_args)]
 #[derive(Debug)]
 enum TaskStatus {
     Todo,
@@ -119,7 +118,7 @@ fn main() {
 
     let mut tasklist = TaskList::new();
     tasklist.add_task(task);
-    println!("{:#?}", tasklist);
+    println!("{:?}", tasklist);
 
     // let todotask = tasklist.todo_count();
     // let inprogresstask = tasklist.inprogress_count();
@@ -141,7 +140,7 @@ fn main() {
     let itemcount = itemlist.count();
     let getallitem = itemlist.get_all();
 
-    println!("items : {:#?}", itemlist);
+    println!("items : {:?}", itemlist);
     println!("items : {}", itemcount);
-    println!("items : {:#?}", getallitem);
+    println!("items : {:?}", getallitem);
 }
