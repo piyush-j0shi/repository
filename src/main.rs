a09e98a396805f2e78e5030210bba3aa66e21709
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-03 14:40:30 +0000

    Generics + Trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+// Generic + Trait
+
+// trait Printable {
+//     fn print(&self);
+// }
+
+// #[derive(Debug)]
+// struct Printer<T> {
+//     name : T,
+// }
+
+// impl<T> Printer<T>
+// where
+//     T : Printable{
+//         fn new(item : T) -> Self {
+//             Printer { name: item }
+//         }
+
+//         fn print_item(&self) {
+//             self.name.print()
+//         }
+//     }
+
 fn main() {
-    println!("i_think_so.rs");
+    println!("ok");
 }
