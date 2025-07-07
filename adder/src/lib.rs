c3a52eefa8ace9c8ec847cf7fb8f0823660f977e
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-07 14:48:04 +0000

    writing custom test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,19 +1,53 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// pub fn add(left: u64, right: u64) -> u64 {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+
+//     #[test]
+//     fn another() {
+//         panic!("make this test fail");
+//     }
+// }
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+#[allow(dead_code)]
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
 
-    #[test]
-    fn another() {
-        panic!("make this test fail");
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller))
     }
 }
