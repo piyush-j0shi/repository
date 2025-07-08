debe2af31c8fb160f2a5a09f52c4eef11eaf9b14
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-08 14:41:45 +0000

    should_panic

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -119,3 +119,30 @@
 //         );
 //     }
 // }
+
+// should_panic
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("value must be between 1 to 100");
+        }
+
+        Guess { value }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
