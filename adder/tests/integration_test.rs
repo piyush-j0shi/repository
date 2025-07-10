01ad4733604824975761f97d01d6633284b6aeae
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-10 14:46:55 +0000

    testcases.rs

--- adder/tests/integration_test.rs
+++ adder/tests/integration_test.rs
@@ -1,10 +1,10 @@
-use adder::add_two;
+// use adder::add_two;
 
-#[test]
-fn it_adds_two() {
-    let result = add_two(3);
-    assert_eq!(result, 5)
-}
+// #[test]
+// fn it_adds_two() {
+//     let result = add_two(3);
+//     assert_eq!(result, 5)
+// }
 
-// to run a particular integration test
-// cargo test --test integration_test
+// // to run a particular integration test
+// // cargo test --test integration_test
