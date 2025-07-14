16ebc8149a3c11fdd1f19f831909604d37d10089
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-14 14:31:52 +0000

    extracting the argument parser

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,16 +5,20 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
+    let (query, file_path) = parse_config(&args);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("searching for {query}");
-    println!("in file {file_path}");
+    // println!("searching for {query}");
+    // println!("in file {file_path}");
 
-    let contents =
-        fs::read_to_string(file_path).expect("something have been able to read the file");
+    // let contents =
+    // fs::read_to_string(file_path).expect("something have been able to read the file");
 
-    println!("with text:\n{contents}");
+    // println!("with text:\n{contents}");
 
-    // modularity
+    (query, file_path)
 }
