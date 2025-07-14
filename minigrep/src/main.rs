db4aba104aa3b7d0f6d34f31330e284d3046604a
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-14 14:47:38 +0000

    creating a constructore for Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,16 +3,12 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
 
-    let config = parse_config(&args);
+    // let contents = fs::read_to_string(config.file_path)
+    //     .expect("Should have been able to read the file");
 
-    // println!("Searching for {}", config.query);
-    // println!("In file {}", config.file_path);
-
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    // println!("content of file is : {}", contents);
+    // println!("content is : {contents}")
 }
 
 struct Config {
@@ -20,9 +16,11 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    Config { query, file_path }
+        Config { query, file_path }
+    }
 }
