3c455f6e0ff151c16eec609b62f08b2d5db66ff6
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-14 15:02:21 +0000

    lib.rs

--- minigrep/target/debug/minigrep.d
+++ minigrep/target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/home/null/repository/minigrep/target/debug/minigrep: /home/null/repository/minigrep/src/main.rs
+/home/null/repository/minigrep/target/debug/minigrep: /home/null/repository/minigrep/src/lib.rs /home/null/repository/minigrep/src/main.rs
