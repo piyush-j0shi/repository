7c9e305973292ca09debd97f991e660ed7d8e274
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-14 14:41:25 +0000

    Grouping Configuration Values

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,22 +3,26 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // dbg!(args);
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
+
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    // println!("content of file is : {}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    // println!("searching for {query}");
-    // println!("in file {file_path}");
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
 
-    // let contents =
-    // fs::read_to_string(file_path).expect("something have been able to read the file");
-
-    // println!("with text:\n{contents}");
-
-    (query, file_path)
+    Config { query, file_path }
 }
