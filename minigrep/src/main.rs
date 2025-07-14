470136b3daee3ad45e6cd1266662fd4d9e4c42f1
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-14 14:53:45 +0000

    handling errors

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
