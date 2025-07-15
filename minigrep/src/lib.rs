f14e67acf12b51a9dc40df4d6361105cd8114cb9
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-15 14:40:12 +0000

    adding functionality

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,7 +22,10 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    // println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
