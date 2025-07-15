0607a19ffcc945577e21b489da6aa74a464d3ebc
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-15 14:33:50 +0000

    wtf lifetimes

---
+++ minigrep/target/debug/deps/minigrep-d801243dead842bad.d
@@ -0,0 +1,5 @@
+/home/null/repository/minigrep/target/debug/deps/minigrep-d801243dead842bad: src/lib.rs
+
+/home/null/repository/minigrep/target/debug/deps/minigrep-d801243dead842bad.d: src/lib.rs
+
+src/lib.rs:
---
+++ minigrep/target/debug/libminigrep.d
@@ -0,0 +1,1 @@
+/home/null/repository/minigrep/target/debug/libminigrep.rlib: /home/null/repository/minigrep/src/lib.rs
