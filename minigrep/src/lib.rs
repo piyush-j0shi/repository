56428040d330fd2a575ab6e43645d670523cb549
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-16 06:14:52 +0000

    adding case_insensitive_search

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -41,6 +41,18 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     result
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line)
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
