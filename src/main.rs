5d85da24dd63b23c8ef73c631e65e54f466d9b8b
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-17 15:08:28 +0000

    closure type inference and annotations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
 #[derive(Debug, PartialEq, Copy, Clone)]
 
 enum ShirtColor {
@@ -55,4 +58,12 @@ fn main() {
         "User 2 gets: {:?} with preference : {:?}",
         giveaway2, user2_preference
     );
+
+    let expensive_closure = |num: u32| -> u32 {
+        println!("calculating slowly");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    println!("Result: {}", expensive_closure(42));
 }
