e088fc0bb680471d468c1b896065d9871117c222
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-18 17:48:31 +0000

    fn tarits in closure

--- src/main.rs
+++ src/main.rs
@@ -112,4 +112,29 @@ fn main() {
     thread::spawn(move || println!("from thread : {list2:?}"))
         .join()
         .unwrap();
+
+    // 1. FnOnce : applies to closures that can be called once. All closures implement at least this trait because all closures can be called
+    // A closure that moves captured values out of its body will only implement FnOnce and none of the other Fn traits,
+    // because it can only be called once.
+
+    // 2. FnMut : applies to closures that don’t move captured values out of their body, but that might mutate the captured values.
+    // These closures can be called more than once.
+
+    // 3. Fn : applies to closures that don’t move captured values out of their body and that don’t mutate captured values,
+    // as well as closures that capture nothing from their environment.
+    // These closures can be called more than once without mutating their environment,
+    // which is important in cases such as calling a closure multiple times concurrently.
+
+    // unwrap_or_else looks like
+    // impl<T> Option<T> {
+    // pub fn unwrap_or_else<F>(self, f: F) -> T
+    // where
+    //     F: FnOnce() -> T
+    // {
+    //     match self {
+    //         Some(x) => x,
+    //         None => f(),
+    //     }
+    // }
+    // }
 }
