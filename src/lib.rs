ed5488b1ce710a91022f6804470efaa6b2b61c27
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-21 14:58:05 +0000

    using closure that capture their environments

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,16 @@ pub trait Iterator {
 // that chip can not go back so with the same analogy we need to think like we can just remove that
 // element after yielding and its not possible without mutablilty.
 
+#[derive(Debug, PartialEq)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoe: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoe.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
 #[test]
 fn iter_demonstrates() {
     let v1 = vec![1, 2, 3];
@@ -26,4 +36,43 @@ fn iter_demonstrates() {
     assert_eq!(v1_iter.next(), Some(&2));
     assert_eq!(v1_iter.next(), Some(&3));
     assert_eq!(v1_iter.next(), None);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
 }
