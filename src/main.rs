3a1adeeb6f9a98c1fde2cbf8af0f8117243f250c
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-21 14:25:54 +0000

    what are the iterators

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// iterators in rust are the patterns that allows you to perform some tasks on a sequence of items
+// in turn.
+
+// but in rust iterators are lazy, meaning they have no effect until you call the method that
+// consumes the iterators to use it up.
+
 fn main() {
     println!("closure.rs");
 }
