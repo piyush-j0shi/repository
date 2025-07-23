828f6a58f2ce0dbcf69f1aeb9ff2378172a1142f
piyush-j0shi <thefarmguy011@gmail.com>
2025-07-23 12:46:03 +0000

    using iterator methods

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -11,12 +11,18 @@ pub struct Config {
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
