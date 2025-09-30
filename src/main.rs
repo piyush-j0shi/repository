b62e0d5245cbe5a77b57ee5558fe0ac49d2f2b32
piyush joshi <thefarmguy011@gmail.com>
2025-09-30 06:41:16 +0000

    using box to store data on heap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("iterators.rs");
+    let b = Box::new(5);
+
+    // modifying the box value
+    // let mut b = Box::new(5);
+    // *b += 5;
+
+    println!("b = {b}");
 }
