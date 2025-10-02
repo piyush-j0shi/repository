a7f71263179c29eafbc3f169c9cdd49e088197e5
piyush joshi <thefarmguy011@gmail.com>
2025-10-02 16:17:03 +0000

    why RefCell<T>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("pointer_rct.rs");
+    // Rc<T> enables the multiple owner of same data.
+    // Box<T> allows immutable and mutable borrow checked at compile time.
+
+    // RefCell<T> allows mutable borrow checked at runtime, you can mutatate the value inside the
+    // RefCall<T> even when the RefCell<T> is immutable.
 }
