38c2e1bcceb5c38bb1623c45fc09753aa1409268
piyush joshi <thefarmguy011@gmail.com>
2025-10-07 17:56:20 +0000

    .join()

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} from spawned thread");
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    handle.join().unwrap();
+
     for i in 1..5 {
         println!("hi number {i} from main thread");
         thread::sleep(Duration::from_millis(1));
     }
+
+    // handle.join().unwrap();
 }
