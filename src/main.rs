7a1a1062ec85530b9d95448f87b339ac64e5f2cb
piyush joshi <thefarmguy011@gmail.com>
2025-10-07 17:44:36 +0000

    thread::swpan

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("weak.rs");
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from spawned thread");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {i} from main thread");
+        thread::sleep(Duration::from_millis(1));
+    }
 }
