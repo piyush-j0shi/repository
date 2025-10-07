9d3ee6bb91bddc6879983e24da7f266baba9ed79
piyush joshi <thefarmguy011@gmail.com>
2025-10-07 18:05:24 +0000

    onlythreads.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::thread;
-use std::time::Duration;
-
 fn main() {
-    let v = vec![1, 2, 3];
-
-    let handle = thread::spawn(move || {
-        println!("here is a vector : {v:?}");
-    });
-
-    handle.join().unwrap();
+    println!("onlythreads.rs");
 }
