6e0ae5d09f50df489e4f265e3088f232025194e3
piyush joshi <thefarmguy011@gmail.com>
2025-10-08 07:03:48 +0000

    onlythreads.rs

--- src/onlythreads.rs
+++ src/onlythreads.rs
@@ -1,10 +1,26 @@
 use std::thread;
+// use std::time::Duration;
 
 fn main() {
-    let v = vec![1, 2, 3];
+    // let handle = thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("number {i} from spawned thread");
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
 
+    // handle.join().unwrap();
+
+    // for i in 1..5 {
+    //     println!("number {i} from main thread");
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    // handle.join().unwrap();
+
+    let value = String::from("this is a string");
     let handle = thread::spawn(move || {
-        println!("here is a vector : {v:?}");
+        println!("string is : {}", value);
     });
 
     handle.join().unwrap();
