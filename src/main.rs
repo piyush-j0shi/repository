c3b6f76fd57b91dc7cf75fab801518f406b0ba16
piyush joshi <thefarmguy011@gmail.com>
2025-10-08 07:04:13 +0000

    onlythreads.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-use std::thread;
-// use std::time::Duration;
-
 fn main() {
-    // let handle = thread::spawn(|| {
-    //     for i in 1..10 {
-    //         println!("number {i} from spawned thread");
-    //         thread::sleep(Duration::from_millis(1));
-    //     }
-    // });
-
-    // handle.join().unwrap();
-
-    // for i in 1..5 {
-    //     println!("number {i} from main thread");
-    //     thread::sleep(Duration::from_millis(1));
-    // }
-
-    // handle.join().unwrap();
-
-    let value = String::from("this is a string");
-    let handle = thread::spawn(move || {
-        println!("string is : {}", value);
-    });
-
-    handle.join().unwrap();
+    println!("onlythreads.rs");
 }
