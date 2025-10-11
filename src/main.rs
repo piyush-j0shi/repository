1386c374de2c585299bf2ec8d1702dd97cc22c14
piyush joshi <thefarmguy011@gmail.com>
2025-10-12 00:13:48 +0530

    Sending Multiple Values and Seeing the Receiver Waiting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use std::{sync::mpsc, thread};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
+    // let (tx, rx) = mpsc::channel();
     let (tx, rx) = mpsc::channel();
 
+    // thread::spawn(move || {
+    //     let val = String::from("hi");
+    //     tx.send(val).unwrap();
+    // });
+
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
     // there are two methods for reciever one is `recv` and another one is `try_recv` and the diffrence is:
@@ -14,6 +31,10 @@ fn main() {
 
     // And the `try_recv` will not hold the main thread for execution if there is a message in queue it will transmit the message othervise it will transmit the error doesn't matter the status of transmitter if there is a message it will transmit and if there is not then it will not block the main thread doesn't matter if the transmitter is active or not.
 
-    let recieved = rx.recv().unwrap();
-    println!("got : {recieved}");
+    // let recieved = rx.recv().unwrap();
+    // println!("got : {recieved}");
+
+    for recieved in rx {
+        println!("got {recieved}");
+    }
 }
