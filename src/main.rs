61da6ff967e202d8183f7ea0b4e31656ca796aa4
piyush joshi <thefarmguy011@gmail.com>
2025-10-11 23:24:19 +0530

    tx, rx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::{sync::mpsc, thread};
+
 fn main() {
-    println!("onlythreads.rs");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+    });
+
+    let recieved = rx.recv().unwrap();
+    println!("got : {recieved}");
 }
