14438990cc01ad5514f457f67d810959ec7feeff
piyush joshi <thefarmguy011@gmail.com>
2025-10-12 00:02:00 +0530

    recv and try_recv

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ fn main() {
         tx.send(val).unwrap();
     });
 
+    // there are two methods for reciever one is `recv` and another one is `try_recv` and the diffrence is:
+
+    // think of this like if messages sent by transmiter are being stored in queue and recv will wait for transmiter that will the transmitter send a message or an error and if the transmitter sends a message it will transmit the message to the reciever and if transmitter sends an error it will transmit the error and for this process it will wait or hold the main thread execution.
+
+    // And the `try_recv` will not hold the main thread for execution if there is a message in queue it will transmit the message othervise it will transmit the error doesn't matter the status of transmitter if there is a message it will transmit and if there is not then it will not block the main thread doesn't matter if the transmitter is active or not.
+
     let recieved = rx.recv().unwrap();
     println!("got : {recieved}");
 }
