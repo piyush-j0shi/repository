a6a7bf68c5e6b6a221334595262674f42eae6d57
piyush joshi <thefarmguy011@gmail.com>
2025-10-14 11:24:56 +0530

    Mutex<T>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::sync::Mutex;
+
 fn main() {
-    println!("mpsc.rs");
+    let m = Mutex::new(5);
+
+    {
+        let mut n = m.lock().unwrap();
+        *n = 6;
+    }
+    // let n = m;
+    println!("m is {:?}", m);
 }
