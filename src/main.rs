71553849ad42b4af4d91f8969a807903a31b8784
piyush joshi <thefarmguy011@gmail.com>
2025-10-14 11:38:08 +0530

    why not Rc

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
+use std::rc::Rc;
 use std::sync::Mutex;
+use std::thread;
 
 fn main() {
-    let m = Mutex::new(5);
+    // Muetx<T> is an abbrevation for Mutual Exclusion and, as in mutex allows only one thread to access some data at any given time. TO access data in Mutex thread first need to accuqire the lock this return MutexGurad<T>
 
-    {
-        let mut n = m.lock().unwrap();
-        *n = 6;
+    // let m = Mutex::new(5);
+
+    // {
+    //     let mut n = m.lock().unwrap();
+    //     *n = 6;
+    // }
+    // // let n = m;
+    // println!("m is {:?}", m);
+
+    let counter = Rc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Rc::clone(&counter);
+
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
-    // let n = m;
-    println!("m is {:?}", m);
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("result : {}", *counter.lock().unwrap());
 }
