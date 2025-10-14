44863e9566871e0bc53ff34d91fbdeda4129f67c
piyush joshi <thefarmguy011@gmail.com>
2025-10-14 14:38:37 +0530

    Arc<Mutex<T>>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::rc::Rc;
-use std::sync::Mutex;
+// use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
@@ -14,12 +14,32 @@ fn main() {
     // // let n = m;
     // println!("m is {:?}", m);
 
-    let counter = Rc::new(Mutex::new(0));
+    // let counter = Rc::new(Mutex::new(0));
+    // let mut handles = vec![];
+
+    // We can not use Rc<T> here because Rc<T> keeps the refrence counts but it does not have concurrency primitives so It can be interrupted by another thread or because of multiple thread
+
+    // for _ in 0..10 {
+    //     let counter = Rc::clone(&counter);
+
+    //     let handle = thread::spawn(move || {
+    //         let mut num = counter.lock().unwrap();
+    //         *num += 1;
+    //     });
+    //     handles.push(handle);
+    // }
+
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+
+    // println!("result : {}", *counter.lock().unwrap());
+
+    let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
     for _ in 0..10 {
-        let counter = Rc::clone(&counter);
-
+        let counter = Arc::clone(&counter);
         let handle = thread::spawn(move || {
             let mut num = counter.lock().unwrap();
             *num += 1;
