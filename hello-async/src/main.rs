201ca80683052609daab3896ada79ab7d7de1dc7
piyush joshi <thefarmguy011@gmail.com>
2025-10-16 16:30:09 +0530

    race

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,4 +1,24 @@
-use trpl::Html;
+use trpl::{Either, Html};
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    trpl::run(async {
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("Its page title was: '{title}'"),
+            None => println!("It had no title."),
+        }
+    })
+}
 
 // async fn block is not like a normal function because it would not run automatically it will return a future so it will not run until you use `.await` with it
 // and once you use `.await` it will return the result.
@@ -11,21 +31,10 @@ use trpl::Html;
 //      }
 // }
 
-async fn page_title(url: &str) -> Option<String> {
-    let reponse_text = trpl::get(url).await.text().await;
-    Html::parse(&reponse_text)
+async fn page_title(url: &str) -> (&str, Option<String>) {
+    let response_text = trpl::get(url).await.text().await;
+    let title = Html::parse(&response_text)
         .select_first("title")
-        .map(|title| title.inner_html())
-}
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-
-    trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} is {title}"),
-            None => println!("{url} had no title"),
-        }
-    })
+        .map(|title| title.inner_html());
+    (url, title)
 }
