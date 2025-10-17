eb10f8aee996f3d5ad67d583353cebd7aaee81ba
piyush joshi <thefarmguy011@gmail.com>
2025-10-17 14:38:18 +0530

    async.rs

---
+++ src/async.rs
@@ -0,0 +1,119 @@
+use std::time::Duration;
+
+fn main() {
+    // using trpl::spawn_task to understand something like thread::spwan 
+    // trpl::run(async {
+    //     let handle = trpl::spawn_task(async {
+    //         for i in 1..10 {
+    //             println!("hi from number {i} from the first task");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     });
+
+    //     for i in 1..5 {
+    //         println!("hi from number {i} from the second task");
+    //         trpl::sleep(Duration::from_millis(500)).await;
+    //     }
+
+    //     handle.await.unwrap();
+    // })
+
+    // using trpl::join instead of .join()
+    // trpl::run (async {
+    //     let fut1 = async {
+    //         for i in 1..10 {
+    //             println!("hi number from {i} from the first task");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //     let fut2 = async {
+    //         for i in 1..5 {
+    //             println!("hi number from {i} from the second task");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //     trpl::join(fut1, fut2).await;
+    // })
+
+    // using channels with trpl 
+    // trpl::run(async {
+    //     let (tx, mut rx) = trpl::channel();
+
+    //     let val = String::from("hello");
+    //     tx.send(val).unwrap();
+
+    //     let recieved = rx.recv().await.unwrap();
+    //     println!("recieved {recieved}");
+    // })
+
+    // trpl::run(async {
+    //     let (tx, mut rx) = trpl::channel();
+
+    //     let tx_fut = async move {
+    //         let vals = vec![
+    //             String::from("hi"),
+    //             String::from("from"),
+    //             String::from("the"),
+    //             String::from("future"),
+    //         ];
+
+    //         for val in vals {
+    //             tx.send(val).unwrap();
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //    let rx_fut = async {
+    //         while let Some(value) = rx.recv().await {
+    //             println!("received '{value}'");
+    //         }
+    //     };
+
+    //     trpl::join(tx_fut, rx_fut).await; 
+    // })
+
+    trpl::run(async {
+                let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("received '{value}'");
+            }
+        };
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
+
+    })
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,3 @@
-use std::time::Duration;
-use trpl::*;
-
 fn main() {
-    trpl::run(async {
-        trpl::swapn_task(async {
-            for i in 1..10 {
-                println!("hi from number {i} from the first task");
-                trpl::sleep(Duration::from_millis(500)).await;
-            }
-        });
-
-        for i in 1..5 {
-            println!("hi from number {i} from the second task");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
-    })  
+    println!("async.rs");
 }
