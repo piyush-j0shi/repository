d8a5bdebe27468c1234f41a1345be8d14e438cf0
piyush joshi <thefarmguy011@gmail.com>
2025-10-17 13:34:12 +0530

    trpl::join

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,20 +1,42 @@
 use std::time::Duration;
 
 fn main() {
-    trpl::run(async {
-        let handle = trpl::spawn_task(async {
+    // using trpl::spawn_task to understand something like thread::spwan 
+
+    // trpl::run(async {
+    //     let handle = trpl::spawn_task(async {
+    //         for i in 1..10 {
+    //             println!("hi from number {i} from the first task");
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     });
+
+    //     for i in 1..5 {
+    //         println!("hi from number {i} from the second task");
+    //         trpl::sleep(Duration::from_millis(500)).await;
+    //     }
+
+    //     handle.await.unwrap();
+    // })
+
+    // using trpl::join instead of .join()
+
+    trpl::run (async {
+        let fut1 = async {
             for i in 1..10 {
-                println!("hi from number {i} from the first task");
+                println!("hi number from {i} from the first task");
                 trpl::sleep(Duration::from_millis(500)).await;
             }
-        });
+        };
 
-        for i in 1..5 {
-            println!("hi from number {i} from the second task");
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
+        let fut2 = async {
+            for i in 1..5 {
+                println!("hi number from {i} from the second task");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
 
-        handle.await.unwrap();
-    })  
+        trpl::join(fut1, fut2).await;
+    })
 }
 
