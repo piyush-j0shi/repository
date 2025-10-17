94881a16d59ebbc412fb37308317d17fb0e7962c
piyush joshi <thefarmguy011@gmail.com>
2025-10-17 13:16:11 +0530

    trpl::spwan_task

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,3 +1,20 @@
+use std::time::Duration;
+
 fn main() {
-    println!("asyncawait.rs");
+    trpl::run(async {
+        let handle = trpl::spawn_task(async {
+            for i in 1..10 {
+                println!("hi from number {i} from the first task");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi from number {i} from the second task");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+
+        handle.await.unwrap();
+    })  
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::time::Duration;
+use trpl::*;
+
 fn main() {
-    println!("asyncawait.rs");
+    trpl::run(async {
+        trpl::swapn_task(async {
+            for i in 1..10 {
+                println!("hi from number {i} from the first task");
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi from number {i} from the second task");
+            trpl::sleep(Duration::from_millis(500)).await;
+        }
+    })  
 }
