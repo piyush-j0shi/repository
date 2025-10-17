d18a4aa098638afc3f34353fd104a1d92dd09ca9
piyush joshi <thefarmguy011@gmail.com>
2025-10-17 14:36:13 +0530

    message passing using trpl

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -48,10 +48,37 @@ fn main() {
     //     println!("recieved {recieved}");
     // })
 
-    trpl::run(async {
-        let (tx, mut rx) = trpl::channel();
+    // trpl::run(async {
+    //     let (tx, mut rx) = trpl::channel();
 
-        let tx_fut = async move {
+    //     let tx_fut = async move {
+    //         let vals = vec![
+    //             String::from("hi"),
+    //             String::from("from"),
+    //             String::from("the"),
+    //             String::from("future"),
+    //         ];
+
+    //         for val in vals {
+    //             tx.send(val).unwrap();
+    //             trpl::sleep(Duration::from_millis(500)).await;
+    //         }
+    //     };
+
+    //    let rx_fut = async {
+    //         while let Some(value) = rx.recv().await {
+    //             println!("received '{value}'");
+    //         }
+    //     };
+
+    //     trpl::join(tx_fut, rx_fut).await; 
+    // })
+
+    trpl::run(async {
+                let (tx, mut rx) = trpl::channel();
+
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
             let vals = vec![
                 String::from("hi"),
                 String::from("from"),
@@ -60,18 +87,33 @@ fn main() {
             ];
 
             for val in vals {
-                tx.send(val).unwrap();
+                tx1.send(val).unwrap();
                 trpl::sleep(Duration::from_millis(500)).await;
             }
         };
 
-       let rx_fut = async {
+        let rx_fut = async {
             while let Some(value) = rx.recv().await {
                 println!("received '{value}'");
             }
         };
 
-        trpl::join(tx_fut, rx_fut).await; 
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(1500)).await;
+            }
+        };
+
+        trpl::join3(tx1_fut, tx_fut, rx_fut).await;
+
     })
 }
 
